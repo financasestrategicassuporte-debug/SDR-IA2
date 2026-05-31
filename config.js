464c3c142924786ec07a2cb0cb2582040f0c6c0a
MaiconMares <47460478+MaiconMares@users.noreply.github.com>
2026-05-30 21:32:12 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  res.json({
-    url: process.env.SUPABASE_URL || '',
-    key: process.env.ANON_KEY || ''
-  });
-}
